f54a698c1cd0abb5507f3ea94e4d4d22cedca0aa
Kanat Sadykov <successkana@gmail.com>
2016-02-16 19:28:27 +0600

    coverflow effect

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
     var swiper = new Swiper('.swiper-container', {
         loop: true,
+        effect: 'coverflow',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true
+        },
         autoplay: 5000
     });
 
     var nextMonday = moment().day(8).format('DD.MM.YYYY');
     $('.sale__next-monday').html(nextMonday + '!');
 });
+
 
